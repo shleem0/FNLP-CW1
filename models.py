3edc24735a641f9a5c3241a76d573c3a9f69321d
samhuggins <@Samuel47-CS>
2025-02-02 16:33:32 +0000

    i think i am getting the hang of git :)

--- models.py
+++ models.py
@@ -241,33 +241,27 @@ class LogisticRegressionClassifier(SentimentClassifier):
         set `self.weights`: [-1.5, 1.25, 1.75]
         set `self.bias`: -0.25
         """
+        
         new_weights = []
-
         loss_gradient_w = [0] * len(self.weights)
+        keys = []
         loss_gradient_b = 0
+
 
         for ex in batch_exs:
 
-            featureCount = self.featurizer.extract_features(ex.words)
+            featureCount = (self.featurizer.extract_features(ex.words))
+            keys.extend(list(featureCount.keys()))
 
             pred = self.predict(ex.words)
-            error = pred - ex.label
+            loss = pred - ex.label 
 
-            i = 0
+            for i, key in enumerate(keys):
+                if key in featureCount:
+                    loss_gradient_w[key] += loss * featureCount[key]
 
-            while len(featureCount) < len(loss_gradient_w):
+            loss_gradient_b += loss
 
-                if i not in featureCount:
-
-                    featureCount.update({i: 0})
-
-                i += 1
-
-            for i, feature in enumerate(featureCount):
-
-                loss_gradient_w[i] += error * featureCount[i]
-
-            loss_gradient_b += error
 
 
         loss_gradient_w = [x / len(batch_exs) for x in loss_gradient_w]
@@ -282,43 +276,6 @@ class LogisticRegressionClassifier(SentimentClassifier):
 
         self.set_weights(new_weights)
         self.set_bias(new_bias)
-
-        # Sam's implementation and test for GitHub
-        
-        # new_weights = []
-        # loss_gradient_w = [0] * len(self.weights)
-        # keys = []
-        # loss_gradient_b = 0
-
-
-        # for ex in batch_exs:
-
-        #     featureCount = (self.featurizer.extract_features(ex.words))
-        #     keys.extend(list(featureCount.keys()))
-
-        #     pred = self.predict(ex.words)
-        #     loss = pred - ex.label 
-
-        #     for i, key in enumerate(keys):
-        #         if key in featureCount:
-        #             loss_gradient_w[key] += loss * featureCount[key]
-
-        #     loss_gradient_b += loss
-
-
-
-        # loss_gradient_w = [x / len(batch_exs) for x in loss_gradient_w]
-        # loss_gradient_b = loss_gradient_b / len(batch_exs)
-
-        # for i, weight in enumerate(self.weights):
-
-        #     new_weight = weight - learning_rate * loss_gradient_w[i]
-        #     new_weights.append(new_weight)
-        
-        # new_bias = self.bias - learning_rate * loss_gradient_b
-
-        # self.set_weights(new_weights)
-        # self.set_bias(new_bias)
 
 
 
@@ -397,8 +354,7 @@ def train_logistic_regression(
         ##########################################
         shuffled_train_exs = []
 
-        for ex in train_exs:
-            shuffled_train_exs.append(ex)
+        shuffled_train_exs = train_exs
         
         np.random.shuffle(shuffled_train_exs)
 
